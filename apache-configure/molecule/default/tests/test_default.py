f2e4faef28ab4413dcbf79f75977a8d4933bcebc
Brian Payne <brian.payne2@gmail.com>
2019-02-16 18:46:44 -0500

    Added molecule testing

--- apache-configure/molecule/default/tests/test_default.py
+++ apache-configure/molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -6,9 +7,30 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize('pkg', [
+  'httpd',
+])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert package.is_installed
+
+
+@pytest.mark.parametrize('svc', [
+  'httpd',
+])
+def test_svc(host, svc):
+    service = host.service(svc)
+
+    assert service.is_running
+    assert service.is_enabled
+
+
+@pytest.mark.parametrize('file, content', [
+  ("/var/www/html/index.html", "Hello World")
+])
+def test_files(host, file, content):
+    file = host.file(file)
+
+    assert file.exists
+    assert file.contains(content)
